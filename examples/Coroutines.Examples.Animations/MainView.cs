5957ea577c35a9226043970bc5d72df403a8fce6
aikidos <www.aikidos@gmail.com>
2019-11-28 07:24:57 +0300

    Small refactoring.

--- examples/Coroutines.Examples.Animations/MainView.cs
+++ examples/Coroutines.Examples.Animations/MainView.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Drawing;
 using System.Windows.Forms;
 using Coroutines.Examples.Animations.Animations;
@@ -7,31 +8,34 @@ namespace Coroutines.Examples.Animations
 {
     public partial class MainView : Form
     {
-        private readonly Timer _updateTimer = new Timer { Interval = 16, Enabled = true };
+        private readonly Timer _updateTimer = new Timer { Interval = 16 };
         private readonly ICoroutineScheduler _scheduler = new CoroutineScheduler();
 
         public MainView()
         {
             InitializeComponent();
-
-            _updateTimer.Tick += (sender, args) => _scheduler.Update();
-
-            _scheduler.Run(ButtonAnimation);
         }
 
-        private IEnumerator<IRoutineReturn> ButtonAnimation()
+        protected override void OnLoad(EventArgs e)
         {
+            base.OnLoad(e);
+
             const int offset = 10;
 
-            var leftTop = new Point(offset, offset);
-            var rightTop = new Point(ClientSize.Width - button.Width - offset, offset);
-            var rightBottom = new Point(rightTop.X, ClientSize.Height - button.Height - offset);
-            var leftBottom = new Point(leftTop.X, rightBottom.Y);
+            var bounds = new Rectangle(new Point(offset, offset), ClientSize - button.Size - new Size(offset * 2, offset * 2));
 
-            yield return Animation.Move(button, rightTop);
-            yield return Animation.Move(button, rightBottom);
-            yield return Animation.Move(button, leftBottom);
-            yield return Animation.Move(button, leftTop);
+            _scheduler.Run(() => Movement(button, bounds));
+
+            _updateTimer.Tick += (sender, args) => _scheduler.Update();
+            _updateTimer.Start();
+        }
+
+        private static IEnumerator<IRoutineReturn> Movement(Control control, Rectangle bounds)
+        {
+            yield return Animation.Move(control, new Point(bounds.Right, bounds.Top));
+            yield return Animation.Move(control, new Point(bounds.Right, bounds.Bottom));
+            yield return Animation.Move(control, new Point(bounds.Left, bounds.Bottom));
+            yield return Animation.Move(control, new Point(bounds.Left, bounds.Top));
 
             // Restart the animation.
             yield return Routine.Reset;
